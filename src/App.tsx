cd2e1347e28d816f726944027e96909b47c4d5a0
Mihovil Santek <msantek@icloud.com>
2020-09-08 19:17:00 +0200

    add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
 
+import React from "react";
+import { Column } from "./Column";
+import { Card } from "./Card";
 import { AppContainer } from "./styles";
 
 const App = () => {
   return (
     <AppContainer>
-      Columns will go here
+      <Column text='To do'>
+        <Card text='Generate app scaffold' />
+      </Column>
+      <Column text='In Progress'>
+        <Card text='Learn TypeScript' />
+      </Column>
+      <Column text='Done'>
+        <Card text='Begin to use static typing' />
+      </Column>
     </AppContainer>
 ) };
 
--- src/styles.ts
+++ src/styles.ts
@@ -1,27 +1,30 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const AppContainer = styled.div`
   align-items: flex-start;
-  background-color: #3179ba
-  display: flex
+  background-color: #3179ba;
+  display: flex;
   flex-direction: row;
-  height: 100%
+  height: 100%;
   padding: 20px;
-  width:100%;`
+  width: 100%;
+`
 
-  export const ColumnContainer = styled.div` 
+
+export const ColumnContainer = styled.div` 
   background-color: #ebecf0;
   width: 300px;
   min-height: 40px;
-  margin-right: 20px; border-radius: 3px;
+  margin-right: 20px;
+  border-radius: 3px;
   padding: 8px 8px;
   flex-grow: 0;`
 
-  export const ColumnTitle = styled.div` 
+export const ColumnTitle = styled.div` 
   padding: 6px 16px 12px; 
   font-weight: bold;`
 
-  export const CardContainer = styled.div` 
+export const CardContainer = styled.div` 
   background-color: #fff;
   cursor: pointer;
   margin-bottom: 0.5rem;
